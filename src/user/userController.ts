5a17a11201b4d7f3cbc5d584e0f741eda22f9a82
Abbas khalil <khalilabbas389@gmail.com>
2024-05-13 17:17:49 +0530

    add error handling in createUser

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -4,44 +4,66 @@ import userModel from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import UserType from "./userType";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
   //^ validation
   if (!name || !email || !password) {
-    const error = createHttpError(400, "all fields are reqired !");
-
+    const error = createHttpError(400, "all fields are reqired !")
     return next(error);
-
-    //   return  res.json({message:"all fields are required!"}) //^ aise bhi kar sakte hai but humne global error handling banaya hai that why iska use nahi karenge
   }
 
   //^check whether user exist or not
-
+try {
+  
   const user = await userModel.findOne({ email });
 
   if (user) {
     const error = createHttpError(400, "user already exists");
-    return next();
+    return next(error);
   }
+  
+} catch (error) {
+
+  return next(createHttpError(500,"error with getting user"))
+  
+}
 
   //^ now lets hash the password
 
   const hashedPass = await bcrypt.hash(password, 10);
+  let newUser:UserType;
+try {
 
-  const newUser = await userModel.create({
+   newUser = await userModel.create({
     name,
     email,
     password: hashedPass,
   });
+  
+} catch (error) {
 
-  //^token generation
+  return next(createHttpError(500,"error while creating user"))
 
-  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
-    expiresIn: "7d",
-  });
+}
 
-  res.json({ accessToken: token });
+
+try {
+    //^token generation
+
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+  
+    res.json({ accessToken: token });
+  
+} catch (error) {
+
+  return next(createHttpError(500,"Error while signing the jwt token"))
+  
+}
+
 };
 
 export { createUser };
