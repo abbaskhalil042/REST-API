2d65fbd9b0f94c91ed844c17a8df1feb45022b73
Abbas khalil <khalilabbas389@gmail.com>
2024-05-16 23:40:50 +0530

    setup multer

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -3,6 +3,8 @@ import { NextFunction, Request, Response } from "express";
 const createBook=async (req: Request, res: Response, next: NextFunction) => {
 
 
+    // const { }
+
     res.json({msg:"book created "})
     
 }
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,10 +1,28 @@
-import express from "express"
-import { createBook } from "./bookController"
+import path from "node:path";
+import express from "express";
+import { createBook } from "./bookController";
+import multer from "multer";
 
-const bookRouter=express.Router()
+const bookRouter = express.Router();
+
+//multer store in local file then we will store that in cloudinary
+const upload = multer({
+  dest: path.resolve(__dirname, "../../public/data/uploads"),
+
+  limits: { fileSize: 3e7 }, //30MB= 30*1024*1024
+});
 
 //
-bookRouter.post("/",createBook)
+bookRouter.post(
+  "/",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    {
+      name: "file",
+      maxCount: 1,
+    },
+  ]),
+  createBook
+);
 
-
-export default bookRouter
+export default bookRouter;
